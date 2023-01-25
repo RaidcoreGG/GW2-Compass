5fa8c6165f06186f6ab7b71ff1c102b35abd7e54
Delta <46466697+DeltaxHunter@users.noreply.github.com>
2023-01-25 21:39:54 +0100

    addon host port + basic implementation for strip

---
+++ src/AddonHost.h
@@ -0,0 +1,161 @@
+#ifndef ADDONHOST_H
+#define ADDONHOST_H
+
+#include <Windows.h>
+#include <d3d11.h>
+#include <mutex>
+
+#include "imgui/imgui.h"
+#include "Mumble.h"
+
+// MinHook Error Codes.
+enum MH_STATUS
+{
+	MH_UNKNOWN = -1,
+	MH_OK = 0,
+	MH_ERROR_ALREADY_INITIALIZED,
+	MH_ERROR_NOT_INITIALIZED,
+	MH_ERROR_ALREADY_CREATED,
+	MH_ERROR_NOT_CREATED,
+	MH_ERROR_ENABLED,
+	MH_ERROR_DISABLED,
+	MH_ERROR_NOT_EXECUTABLE,
+	MH_ERROR_UNSUPPORTED_FUNCTION,
+	MH_ERROR_MEMORY_ALLOC,
+	MH_ERROR_MEMORY_PROTECT,
+	MH_ERROR_MODULE_NOT_FOUND,
+	MH_ERROR_FUNCTION_NOT_FOUND
+};
+
+typedef MH_STATUS(__stdcall*MINHOOK_CREATE)(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal);
+typedef MH_STATUS(__stdcall*MINHOOK_REMOVE)(LPVOID pTarget);
+typedef MH_STATUS(__stdcall*MINHOOK_ENABLE)(LPVOID pTarget);
+typedef MH_STATUS(__stdcall*MINHOOK_DISABLE)(LPVOID pTarget);
+
+struct VTableMinhook
+{
+	MINHOOK_CREATE		CreateHook;
+	MINHOOK_REMOVE		RemoveHook;
+	MINHOOK_ENABLE		EnableHook;
+	MINHOOK_DISABLE		DisableHook;
+};
+
+enum class ELogLevel : unsigned char
+{
+	OFF = 0,
+	CRITICAL = 1,
+	WARNING = 2,
+	INFO = 3,
+	DEBUG = 4,
+	TRACE = 5,
+	ALL
+};
+
+typedef struct LogEntry
+{
+	ELogLevel LogLevel;
+	unsigned long long Timestamp;
+	std::wstring Message;
+
+	std::wstring TimestampString(bool aIncludeDate = true);
+	std::wstring ToString();
+};
+
+class ILogger
+{
+	public:
+	ILogger() = default;
+	virtual ~ILogger() = default;
+
+	ELogLevel GetLogLevel();
+	void SetLogLevel(ELogLevel aLogLevel);
+
+	virtual void LogMessage(LogEntry aLogEntry) = 0;
+
+	protected:
+	ELogLevel LogLevel;
+	std::mutex MessageMutex;
+};
+
+typedef void (*LOGGER_LOGA)(ELogLevel aLogLevel, const char* aFmt, ...);
+typedef void (*LOGGER_LOGW)(ELogLevel aLogLevel, const wchar_t* aFmt, ...);
+typedef void (*LOGGER_ADDREM)(ILogger* aLogger);
+
+struct VTableLogging
+{
+	LOGGER_LOGA			LogA;
+	LOGGER_LOGW			LogW;
+	LOGGER_ADDREM		RegisterLogger;
+	LOGGER_ADDREM		UnregisterLogger;
+};
+
+typedef void (*EVENTS_CONSUME)(void* aEventArgs);
+typedef void (*EVENTS_RAISE)(const wchar_t* aEventName, void* aEventData);
+typedef void (*EVENTS_SUBSCRIBE)(const wchar_t* aEventName, EVENTS_CONSUME aConsumeEventCallback);
+
+struct Keybind
+{
+	WORD Key;
+	bool Alt;
+	bool Ctrl;
+	bool Shift;
+};
+
+typedef void (*KEYBINDS_PROCESS)(const wchar_t* aIdentifier);
+typedef void (*KEYBINDS_REGISTER)(const wchar_t* aIdentifier, KEYBINDS_PROCESS aKeybindHandler, Keybind aKeybind);
+typedef void (*KEYBINDS_UNREGISTER)(const wchar_t* aIdentifier);
+
+struct AddonAPI
+{
+	IDXGISwapChain* SwapChain;
+	ImGuiContext* ImguiContext;
+	LinkedMem* MumbleLink;
+
+	VTableMinhook		MinhookFunctions;
+	VTableLogging		LoggingFunctions;
+
+	/* Events */
+	EVENTS_RAISE		RaiseEvent;
+	EVENTS_SUBSCRIBE	SubscribeEvent;
+
+	/* Keybinds */
+	KEYBINDS_REGISTER	RegisterKeybind;
+	KEYBINDS_UNREGISTER	UnregisterKeybind;
+
+	/* API */
+		// GW2 API FUNCS
+		// LOGITECH API FUNCS
+		// RESOURCE SHARING FUNCS
+};
+
+enum class EUpdateProvider
+{
+	None = 0,		/* Does not support auto updating */
+	Raidcore = 1,	/* Provider is Raidcore (via API) */
+	GitHub = 2,		/* Provider is GitHub Releases */
+	Direct = 3		/* Provider is direct file link */
+};
+
+typedef void (*ADDON_LOAD)(AddonAPI aHostApi);
+typedef void (*ADDON_UNLOAD)();
+typedef void (*ADDON_RENDER)();
+typedef void (*ADDON_OPTIONS)();
+
+struct AddonDefinition
+{
+    signed int      Signature;      /* Raidcore Addon ID, set to random negative integer if not on Raidcore */
+    const wchar_t*  Name;           /* Name of the addon as shown in the library */
+    const wchar_t*  Version;        /* Leave as `__DATE__ L" " __TIME__` to maintain consistency */
+    const wchar_t*  Author;         /* Author of the addon */
+    const wchar_t*  Description;    /* Short description */
+    ADDON_LOAD      Load;           /* Pointer to Load Function of the addon */
+    ADDON_UNLOAD    Unload;         /* Pointer to Unload Function of the addon */
+
+    ADDON_RENDER    Render;         /* Present callback to render imgui */
+    ADDON_OPTIONS   Options;        /* Options window callback, called when opening options for this addon */
+
+    EUpdateProvider Provider;       /* What platform is the the addon hosted on */
+    const wchar_t*  UpdateLink;     /* Link to the update resource */
+};
+
+#endif
--- src/entry.cpp
+++ src/entry.cpp
@@ -1,13 +1,9 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#pragma comment(lib, "winmm.lib")
-#include <stdint.h>
-#include <stdio.h>
 #include <Windows.h>
-#include <D3D9.h>
-#include "arcdps_structs.h"
-#include "imgui\imgui.h"
 #include <string>
-#include "mumble.h"
+#include <cmath>
+
+#include "AddonHost.h"
+#include "imgui\imgui.h"
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
@@ -21,218 +17,164 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     return TRUE;
 }
 
-/* proto */
-extern "C" __declspec(dllexport) void* get_init_addr(char* arcversion, ImGuiContext* imguictx, IDirect3DDevice9* id3dd9, HANDLE arcdll, void* mallocfn, void* freefn);
-extern "C" __declspec(dllexport) void* get_release_addr();
+AddonAPI APIDefs;
+AddonDefinition* AddonDef;
 
-arcdps_exports* mod_init();
-uintptr_t mod_release();
-uintptr_t mod_imgui(uint32_t not_charsel_or_loading); /* id3dd9::present callback, before imgui::render, fn(uint32_t not_charsel_or_loading) */
-uintptr_t mod_options(); /* id3dd9::present callback, appending to the end of options window in arcdps, fn() */
-uintptr_t mod_options_windows(const char* windowname); /* id3dd9::present callback, appending to the end of options window in arcdps, fn() */
+bool IsCompassStripVisible = false;
+bool IsWorldCompassVisible = false;
+LinkedMem* MumbleLink = nullptr;
 
-void log(char* str);
-void log_file(char* str);
-void log_arc(char* str);
-
-/* globals */
-arcdps_exports arc_exports;
-IDirect3DDevice9* d3d9device;
-void* filelog;
-void* arclog;
-
-bool show_mumble = false;
-bool show_compass_strip = false;
-bool show_compass_world = false;
-LinkedMem* p_Mumble = nullptr;
-
-/* log to arcdps.log and log window*/
-void log(char* str)
+void ProcessKeybind(const wchar_t* aIdentifier)
 {
-	log_file(str);
-	log_arc(str);
-	return;
-}
-/* log to arcdps.log, thread/async safe */
-void log_file(char* str)
-{
-	size_t(*log)(char*) = (size_t(*)(char*))filelog;
-	if (log) (*log)(str);
-	return;
-}
-/* log to extensions tab in arcdps log window, thread/async safe */
-void log_arc(char* str)
-{
-	size_t(*log)(char*) = (size_t(*)(char*))arclog;
-	if (log) (*log)(str);
-	return;
-}
-
-/* export -- arcdps looks for this exported function and calls the address it returns on client load */
-extern "C" __declspec(dllexport) void* get_init_addr(char* arcversion, ImGuiContext* imguictx, IDirect3DDevice9* id3dd9, HANDLE arcdll, void* mallocfn, void* freefn)
-{
-	ImGui::SetCurrentContext((ImGuiContext*)imguictx);
-	ImGui::SetAllocatorFunctions((void *(*)(size_t, void*))mallocfn, (void(*)(void*, void*))freefn); // on imgui 1.80+
-
-	d3d9device = id3dd9;
-	filelog = (void*)GetProcAddress((HMODULE)arcdll, "e3");
-	arclog = (void*)GetProcAddress((HMODULE)arcdll, "e8");
-
-	return mod_init;
-}
-/* export -- arcdps looks for this exported function and calls the address it returns on client exit */
-extern "C" __declspec(dllexport) void* get_release_addr()
-{
-	return mod_release;
-}
-
-/* initialize mod -- return table that arcdps will use for callbacks. exports struct and strings are copied to arcdps memory only once at init */
-arcdps_exports* mod_init()
-{
-	/* for arcdps */
-	memset(&arc_exports, 0, sizeof(arc_exports));
-	arc_exports.sig = 0x2B5EC1B9;
-	arc_exports.imguivers = IMGUI_VERSION_NUM;
-	arc_exports.size = sizeof(arc_exports);
-	arc_exports.out_name = "Compass";
-	arc_exports.out_build = "2021-06-26";
-	arc_exports.imgui = mod_imgui;
-	//arc_exports.options_end = mod_options;
-	arc_exports.options_windows = mod_options_windows;
-
-	log((char*)"Compass: MOD_INIT"); // if using vs2015+, project properties > c++ > conformance mode > permissive to avoid const to not const conversion error
-
-	p_Mumble = mumble_link_create();
-
-	return &arc_exports;
-}
-/* release mod -- return ignored */
-uintptr_t mod_release()
-{
-	mumble_link_destroy();
-	return 0;
-}
-
-uintptr_t mod_imgui(uint32_t not_charsel_or_loading)
-{
-	if (show_mumble)
+	/* if COMPASS_TOGGLEVIS is passed, we toggle the compass visibility */
+	if (wcscmp(aIdentifier, L"COMPASS_TOGGLEVIS") == 0)
 	{
-		char buffer[4096];
-		char* p = &buffer[0];
-
-		p += _snprintf_s(p, 400, _TRUNCATE, "== INTERFACE ==\n");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "version", p_Mumble->ui_version);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "tick", p_Mumble->ui_tick);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "mapopen", p_Mumble->IsMapOpen ? "true" : "false");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "focus", p_Mumble->IsGameFocused ? "true" : "false");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "pvp", p_Mumble->IsCompetitive ? "true" : "false");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "txtfocus", p_Mumble->IsTextboxFocused ? "true" : "false");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "combat", p_Mumble->IsInCombat ? "true" : "false");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "cmploc", p_Mumble->IsCompassTopRight ? "top" : "bottom");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "cmpwidth", p_Mumble->compassWidth);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "cmpheight", p_Mumble->compassHeight);
-		if (p_Mumble->IsCompassRotating)
-		{
-			p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %09.4f\n", "cmprot", p_Mumble->compassRotation);
-		}
-		else
-		{
-			p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "cmprot", "disabled");
-		}
-		float camrotdeg = atan2f(p_Mumble->cam_front.x, p_Mumble->cam_front.z) * 180 / 3.14159265f; // gets the rotation in degrees; north = 0, clock-wise to 360
-		if (camrotdeg < 0) { camrotdeg += 360; }
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %09.4f\n", "mapx", p_Mumble->mapCenterX);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %09.4f\n", "mapy", p_Mumble->mapCenterY);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %09.4f\n", "mapscale", p_Mumble->mapScale);
-		p += _snprintf_s(p, 400, _TRUNCATE, "\n");
-
-		p += _snprintf_s(p, 400, _TRUNCATE, "== PLAYER ==\n");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "posx", p_Mumble->avatar_pos.x);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "posy", p_Mumble->avatar_pos.y);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "posz", p_Mumble->avatar_pos.z);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "frontx", p_Mumble->avatar_front.x);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "fronty", p_Mumble->avatar_front.y);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "frontz", p_Mumble->avatar_front.z);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "gx", p_Mumble->playerX);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "gy", p_Mumble->playerY);
-		std::string mount;
-		switch (p_Mumble->mountIndex)
-		{
-			case 0x00: mount = "Unmounted"; break;
-			case 0x01: mount = "Jackal"; break;
-			case 0x02: mount = "Griffon"; break;
-			case 0x03: mount = "Springer"; break;
-			case 0x04: mount = "Skimmer"; break;
-			case 0x05: mount = "Raptor"; break;
-			case 0x06: mount = "RollerBeetle"; break;
-			case 0x07: mount = "Warclaw"; break;
-			case 0x08: mount = "Skyscale"; break;
-		}
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "mount", mount.c_str());
-		p += _snprintf_s(p, 400, _TRUNCATE, "\n");
-
-		p += _snprintf_s(p, 400, _TRUNCATE, "== CAMERA ==\n");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "posx", p_Mumble->cam_pos.x);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "posy", p_Mumble->cam_pos.y);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "posz", p_Mumble->cam_pos.z);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "frontx", p_Mumble->cam_front.x);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "fronty", p_Mumble->cam_front.y);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %+09.4f\n", "frontz", p_Mumble->cam_front.z);
-		p += _snprintf_s(p, 400, _TRUNCATE, "\n");
-
-		p += _snprintf_s(p, 400, _TRUNCATE, "== GAME ==\n");
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "mapid", p_Mumble->mapId);
-		std::string mapType;
-		switch (p_Mumble->mapType)
-		{
-			case 0x00: mapType = "AutoRedirect"; break;
-			case 0x01: mapType = "CharacterCreation"; break;
-			case 0x02: mapType = "PvP"; break;
-			case 0x03: mapType = "GvG"; break;
-			case 0x04: mapType = "Instance"; break;
-			case 0x05: mapType = "PvE"; break;
-			case 0x06: mapType = "Tournament"; break;
-			case 0x07: mapType = "Tutorial"; break;
-			case 0x08: mapType = "UserTournament"; break;
-			case 0x09: mapType = "WvW_EBG"; break;
-			case 0x0A: mapType = "WvW_BBL"; break;
-			case 0x0B: mapType = "WvW_GBL"; break;
-			case 0x0C: mapType = "WvW_RBL"; break;
-			case 0x0D: mapType = "WvW_FV"; break;
-			case 0x0E: mapType = "WvW_OS"; break;
-			case 0x0F: mapType = "WvW_EOTM"; break;
-			case 0x10: mapType = "PvE_Mini"; break;
-			case 0x11: mapType = "BIG_BATTLE"; break;
-			case 0x12: mapType = "WvW_Lounge"; break;
-			case 0x13: mapType = "WvW"; break;
-		}
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %s\n", "maptype", mapType.c_str());
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u.%u.%u.%u\n", "ip",
-			p_Mumble->serverAddress[4], p_Mumble->serverAddress[5],
-			p_Mumble->serverAddress[6], p_Mumble->serverAddress[7]);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "shard", p_Mumble->shardId);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "instance", p_Mumble->instance);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "build", p_Mumble->buildId);
-		p += _snprintf_s(p, 400, _TRUNCATE, "%-010s %u\n", "pid", p_Mumble->processId);
-
-		//ImGui::ShowDemoWindow();
-		ImGui::Begin("== MUMBLE ==", 0, 99);
-		ImGui::Text(buffer);
-		ImGui::End();
+		IsCompassStripVisible = !IsCompassStripVisible;
+		return;
 	}
-
-	return 0;
 }
 
-uintptr_t mod_options()
+void SetupKeybinds()
 {
-	ImGui::Checkbox("Mumble", &show_mumble);
-	return 0;
+	Keybind ctrlC{};
+	ctrlC.Ctrl = true;
+	ctrlC.Key = 67;
+	APIDefs.RegisterKeybind(L"COMPASS_TOGGLEVIS", ProcessKeybind, ctrlC);
 }
 
-uintptr_t mod_options_windows(const char* windowname) {
-	if (!windowname) {
-		mod_options();
+void AddonLoad(AddonAPI aHostApi)
+{
+	APIDefs = aHostApi;
+	ImGui::SetCurrentContext(aHostApi.ImguiContext);
+	//ImGui::SetAllocatorFunctions((void* (*)(size_t, void*))mallocfn, (void(*)(void*, void*))freefn); // on imgui 1.80+
+
+	MumbleLink = APIDefs.MumbleLink;
+
+	SetupKeybinds();
+}
+
+void AddonUnload()
+{
+	APIDefs.UnregisterKeybind(L"COMPASS_TOGGLEVIS");
+	/* free some resources */
+}
+
+std::string GetMarkerText(int aRotation, bool notch = true)
+{
+	// adjust for 0-180 -180-0 -> 0-359
+	if (aRotation < 0) { aRotation += 360; }
+	if (aRotation == 360) { aRotation = 0; }
+
+    std::string marker;
+
+	switch (aRotation)
+	{
+		case   0: marker.append("N"); break;
+		case  45: marker.append("NE"); break;
+		case  90: marker.append("E"); break;
+		case 135: marker.append("SE"); break;
+		case 180: marker.append("S"); break;
+		case 225: marker.append("SW"); break;
+		case 270: marker.append("W"); break;
+		case 315: marker.append("NW"); break;
+		default:  marker.append(notch ? "|" : std::to_string(aRotation)); break;
 	}
-	return 0;
+
+    return marker;
+}
+
+float widgetWidth = 600.0f;
+float range = 180.0f;
+float step = 15.0f;
+float centerX = widgetWidth / 2;
+float offsetPerDegree = widgetWidth / range;
+
+void AddonRender()
+{
+	if (!IsCompassStripVisible) { return; }
+
+	ImGui::PushItemWidth(widgetWidth);
+    if (ImGui::Begin("COMPASS_STRIP", (bool *)0, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoTitleBar))
+    {
+        float fRot = atan2f(MumbleLink->CameraFront.X, MumbleLink->CameraFront.Z) * 180.0f / 3.14159f;
+        int iRot = round(fRot);
+
+		ImGui::Separator();
+
+        ImVec2 initial = ImGui::GetCursorPos();
+		initial.x = 0;
+
+		/* this is to display text between 0-360 */
+        std::string cText = GetMarkerText(iRot, false);
+
+		/* center scope */
+        ImVec2 scope = initial;
+        scope.x += centerX;
+		float scopeWidth = ImGui::CalcTextSize(cText.c_str()).x / 2;
+        scope.x -= scopeWidth; // center the marker
+        ImGui::SetCursorPos(scope);
+		ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(221, 153, 0, 255));
+        ImGui::Text(cText.c_str());
+		ImGui::PopStyleColor();
+		
+		for (size_t i = 0; i < 360 / step; i++)
+		{
+			float fMarker = step * i;
+			if (fMarker > 180) { fMarker -= 360; } // correct so we have negative wrapping again instead of 0-360;
+			int iMarker = round(fMarker);
+
+			/* this is to display text between 0-360 */
+			std::string notchText = GetMarkerText(iMarker, false);
+
+			ImVec2 pos = initial;
+			pos.x += centerX;
+			float offset = offsetPerDegree * (fMarker + (fRot * (-1)));
+			
+			/* fix behind */
+			if (offset > (widgetWidth * 1.5)) { offset -= (widgetWidth * 2); }
+			else if (offset < (widgetWidth * 0.5 * -1)) { offset += (widgetWidth * 2); }
+
+			float centerPreText = offset;
+			float markerWidth = ImGui::CalcTextSize(notchText.c_str()).x / 2;
+			offset -= markerWidth; // center the marker
+			pos.x += offset;
+
+			// + 5.0f "padding"
+			float centerLow = initial.x - (scopeWidth + markerWidth + 5.0f);
+			float centerHigh = initial.x + (scopeWidth + markerWidth + 5.0f);
+			if (centerPreText > centerLow && centerPreText < centerHigh) { continue; }
+
+			ImGui::SetCursorPos(pos);
+			ImGui::Text(notchText.c_str());
+		}
+    }
+
+    ImGui::End();
+}
+
+void AddonOptions()
+{
+	/* This addon has no options *shrug* */
+	return;
+}
+
+extern "C" __declspec(dllexport) AddonDefinition * GetAddonDef()
+{
+	AddonDef = new AddonDefinition();
+	AddonDef->Signature = 17;
+	AddonDef->Name = L"World Compass";
+	AddonDef->Version = __DATE__ L" " __TIME__;
+	AddonDef->Author = L"Raidcore";
+	AddonDef->Description = L"Adds a simple compass widget to the UI, as well as to your character in the world.";
+	AddonDef->Load = AddonLoad;
+	AddonDef->Unload = AddonUnload;
+
+	AddonDef->Render = AddonRender;
+	AddonDef->Options = AddonOptions;
+
+	/* not necessary if hosted on Raidcore, but shown anyway for the  example also useful as a backup resource */
+	AddonDef->Provider = EUpdateProvider::GitHub;
+	AddonDef->UpdateLink = L"https://github.com/RaidcoreGG/GW2-Compass";
+
+	return AddonDef;
 }
